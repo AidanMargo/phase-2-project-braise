903c495165a1bbf36b1ea3090323cf325ec54016
Avery Lin <tclin100@gmail.com>
2021-10-28 11:40:38 -0400

    added type dropdown

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,8 @@ function Form ({addRecipeToState}) {
     name:'',
     image:'',
     ingredients:'',
-    instructions:''
+    instructions:'',
+    type:''
   })
 
   const handleChange = (e) => setFormData({...formData, [e.target.name]: e.target.value})
@@ -20,18 +21,29 @@ function Form ({addRecipeToState}) {
 
 
   return (
+
     <div id="form-container">
       <form id="new-recipe-form">
         <label htmlFor="dishname">Name:</label><input class ="form-input" id="dishname" name="name" value={formData.dishname} onChange={handleChange}/>
         <label htmlFor="image">Image:</label><input class ="form-input" id="image" name="image" value={formData.image} onChange={handleChange}/>
         <label htmlFor="ingredients">Ingredients:</label> <input class ="form-input" id="ingredient-input" name="ingredients" value={formData.ingredients} onChange={handleChange} placeholder="Comma is required between each ingredient"/>
         <label htmlFor="instructions">Instructions:</label> <input class ="form-input" id="instruction-input" name="instructions" value={formData.instructions} onChange={handleChange}/>
+        <select onChange={handleChange}>Type:
+            <option value=''></option>
+            <option value='American'>American</option>
+            <option value='Chinese'>Chinese</option>
+            <option value='Thai'>Thai</option>
+            <option value='Mexican'>Mexican</option>
+            <option value='Italian'>Italian</option>
+            </select>
         <Link to="/recipes">
           <button onClick={(e) => addRecipe(e)}>Submit</button>
         </Link>
-      </form>
+
+        </form>
 
     </div>
+
   )
 }
 
